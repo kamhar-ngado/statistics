47da0d6d4f9b85b5042a9acc0483708fbb0ae29f
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-10-21 13:38:40 +0800

    add files

--- materi2.py
+++ materi2.py
@@ -1,6 +1,10 @@
 import statistics
+from statistics import mode
+from statistics import variance
 
-data = [1, 2, 3, 4, 5]
-x = statistics.mean(data)
-print("x=", x)
-print(data[1])
+# data = [1, 2, 3, 4, 5]
+# Median = statistics.median(data)
+# print("median = ", Median)
+
+data1 = [2, 3, 3, 4, 5, 5, 5, 5, 6, 6, 6, 7]
+print(statistics.mode(data1))
