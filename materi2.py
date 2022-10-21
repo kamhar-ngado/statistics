0bc174e3d01757c6ea7241d8ddf6eb37523a981c
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-10-21 13:18:45 +0800

    add files

---
+++ materi2.py
@@ -0,0 +1,4 @@
+import statistics
+
+data = [1, 2, 3, 4, 5]
+print(statistics.mean(data))
---
+++ test2.py
@@ -0,0 +1,23 @@
+import math
+nama= "kamhar ngado"
+# print(nama)
+# print(nama)
+# print(nama)
+
+for x in range(5):
+    print(x+1, nama)
+data = [1, 2, 3, 4, 5]
+sum = (1 + 2+ 3+ 4 +5)/5
+print("mean = ", sum)
+
+# for i in range(0, len(data)):
+#     sum = sum+ data[i]
+
+
+
+
+    
+
+
+
+
